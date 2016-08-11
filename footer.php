aab111c8b2bffa3ce3310416d32673aeaa4996ea
contact@alexwen.me <contact@alexwen.me>
2016-08-11 04:04:06 -0500

    Fix mobile fonts, update footer.

--- footer.php
+++ footer.php
@@ -15,12 +15,15 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
     <div class="footer-bio">
-      <h2>artisticritique</h2>
+      <span class="footer-span darkwhite">artisticritique</span>
       <p>artisticritique is a media journal chronicling interesting music, film and other mediums of artistic expression. It's an evaluation of the power and value of the contemporary arts, tackling modern media with a critical lens.
       </p>
     </div>
 		<div class="alex-bio">
-      <h2>Alex Wen</h2>
+      <a href="http://alexwen.xyz"><div class="mini-name-box">
+        <span class="footer-title">ALEX</span>
+        <span class="footer-title">WEN</span>
+      </div></a>
       <p> Writing & web design by Alex with assistance from <a href="http://wordpress.org">Wordpress</a> & <a href="http://underscores.me">Underscores</a>.</p>
       <span><a href="http://alexwen.xyz">Portfolio</a> //
       <a href="http://twitter.com/thatnewwen">Twitter</a> //
